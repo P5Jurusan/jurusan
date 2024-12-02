5639c2ec239cb7e2fc1273488a104e9aa8ef071f
abizar0016 <muhammadabizar0016@gmail.com>
2024-12-02 10:33:01 +0700

    add

--- app.py
+++ app.py
@@ -1,51 +1,89 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+import secrets
+from flask_sqlalchemy import SQLAlchemy
+
+
 
 app = Flask(__name__)
+app.secret_key = secrets.token_hex(32)
+
+# Konfigurasi Database SQLite
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///jurusan.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    nip = db.Column(db.String(20), unique=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+def init_db():
+    db.create_all()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        nip = request.form.get('nip')
+        password = request.form.get('password')
+        
+        #validasi
+        if not nip or not nip.isdigit() or len(nip) != 18:
+            flash('NIP harus terdiri dari 18 angka!', 'danger')
+            return redirect(url_for('login'))
+        
+        if not password or len(password) < 8:
+            flash('Password harus terdiri dari minimal 8 karakter!', 'danger')
+            return redirect(url_for('login'))
+
+        # Cek NIP dan Password
+        user = User.query.filter_by(nip=nip, password=password).first()
+        if user:
+            session['user'] = user.name
+            flash('Login berhasil!', 'success')
+            return redirect(url_for('admin'))
+        else:
+            flash('NIP atau password salah!', 'danger')
+            return redirect(url_for('login'))
+    
     return render_template('login.html')
 
-@app.route('/register')
+@app.route('/register', methods=['POST'])
 def register():
+    if request.method == 'POST':
+        nip = request.form.get('nip')
+        name = request.form.get('name')
+        password = request.form.get('password')
+        
+        #validasi
+        if not nip or not nip.isdigit() or len(nip) != 18:
+            flash('NIP harus terdiri dari 18 angka!', 'danger')
+            return redirect(url_for('register'))
+        
+        if not name or len(name) < 3:
+            flash('Nama harus terdiri dari minimal 3 karakter!', 'danger')
+            return redirect(url_for('register'))
+        
+        if not password or len(password) < 8:
+            flash('Password harus terdiri dari minimal 8 karakter!', 'danger')
+            return redirect(url_for('register'))
+
+        # Simpan data ke database
+        new_user = User.query.filter_by(nip=nip, name=name, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('Registrasi berhasil!', 'success')
+        return redirect(url_for('login'))
     return render_template('register.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/service')
-def service():
-    return render_template('service.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
-@app.route('/project')
-def project():
-    return render_template('project.html')
-
-@app.route('/team')
-def team():
-    return render_template('team.html')
-
-@app.route('/testimonial')
-def testimonial():
-    return render_template('testimonial.html')
-
-@app.route('/author')
-def author():
-    return render_template('authorize.html')
-
-@app.route('/404')
-def error():
-    return render_template('404.html')
-
 if __name__ == '__main__':
+    with app.app_context():
+        init_db()
     app.run(debug=True)
-    
